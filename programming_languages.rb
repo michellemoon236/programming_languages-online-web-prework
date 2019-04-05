007999f8e4d8f02828df55c83872b7a266ef3ed6
Michelle M <michellemoon236@gmail.com>
2019-04-05 03:24:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
+
+  
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, description|
+    description.each do |language, type|
+        new_hash[language] = type
+        new_hash[language][:style] = [style]
+    end
+  end
+   new_hash  
+   binding.pry
 end
+
