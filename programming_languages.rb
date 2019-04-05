85232eae7a39e25636ee632906f2fc0fb7e60790
Michelle M <michellemoon236@gmail.com>
2019-04-05 03:30:31 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -6,12 +6,11 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, description|
     description.each do |language, type|
-        new_hash[language] = type
-        new_hash[language][:style] = [style]
         if new_hash.has_key?(language)
           new_hash[language][:style] << style
         else
-          
+          new_hash[language] = type
+          new_hash[language][:style] = [style]
         end
     end
   end
